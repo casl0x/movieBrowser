bc83b4291f9b18850aee0f94adcd196128efe4aa
Cassidy <cassidy.rouelle@gmail.com>
2024-04-16 09:39:08 +0200

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,15 +5,13 @@ export default function Footer() {
     <footer >
       <a href="https://www.themoviedb.org/" className='tmdb'><img src="/logo-short.svg" alt="logo of TMDB" /></a>        
       <div className='api'>
-        <p></p>
         <a href="https://developer.themoviedb.org/docs/faq" className='link'>About TMDB</a>        
         <a href="https://www.themoviedb.org/terms-of-use" className='link'>Terms of use</a>
         <a href="https://www.themoviedb.org/api-terms-of-use" className='link'>API terms of use</a>
       </div>
       <div className='developer'>
-        <p></p>
         <a href="" className='link'>About the developer</a>
-        <a href="https://github.com/casl0x/movieBrowser">Repository of the project</a>        
+        <a href="https://github.com/casl0x/movieBrowser" className='link'>Repository of the project</a>        
       </div>
     </footer>
   )
--- src/utils/request.jsx
+++ src/utils/request.jsx
@@ -1,5 +1,3 @@
-import { useEffect, useState } from "react";
-
 let API_URL = import.meta.env.VITE_REACT_APP_API_URL;
 let API_KEY = import.meta.env.VITE_REACT_APP_API_KEY;
 let ACCESS_TOKEN = import.meta.env.VITE_REACT_APP_ACCESS_TOKEN;
@@ -8,18 +6,18 @@ export async function FetchBanner() {
     const options = {
         method: 'GET',
         headers: {
-        accept: 'application/json',
-        Authorization: `Bearer ${ACCESS_TOKEN}`
+            accept: 'application/json',
+            Authorization: `Bearer ${ACCESS_TOKEN}`
         }
     };
             
     try {
-        const response = await fetch(`${API_URL}authentication&${API_KEY}`, options);
+        const response = await fetch(`${API_URL}movie/top_rated?api_key=${API_KEY}`, options);
         if (!response.ok) {
         throw new Error('Network response was not ok');
         }
         const data = await response.json();
-        return data.result.slice(0, 10)
+        return data.results.slice(0, 10)
     } catch (error) {
         console.error(error)
         throw new Error("Can't fetch the datas")
