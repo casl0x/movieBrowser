03dbbaba9523e0017d50d7eadbd9de60b4a0fa74
Cassidy <cassidy.rouelle@gmail.com>
2024-04-16 15:36:10 +0200

    navbar, responsive to upgrade

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,19 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Favorite from './pages/Favorite';
 import WatchList from './pages/WatchList';
-import About from './pages/About';
 
 // components
-import Header from './components/Header';
+import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
 export default function App() {
   return (
     <div className="app">
-        <Header />
+        <Navbar />
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/favorite" element={<Favorite />} />
             <Route path="/watchlist" element={<WatchList />} />
-            <Route path="/about" element={<About />} />
         </Routes>
         <Footer />
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { useMediaQuery } from 'react-responsive'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHouse, faInfo, faPlay, faStar } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
 
-import Search from './Search'
 
 export default function Navbar() {
+  const [isActive, setIsActive] = useState(false);
+  const isMobile = useMediaQuery({maxWidth: "768px"});
+  const listClassName = isMobile ? "navbar-list" : "dk-nav-list";
+  const iconClassName = isMobile ? "toggle" : "no-icon"
+
+  const toggleActive = () => {
+    setIsActive(!isActive);
+  }
+
   return (
-    <nav>
-      <Link to="/">
-        <FontAwesomeIcon icon={faHouse} className="icon favorite" />
-      </Link>
-      <Link to="/favorite">
-        <FontAwesomeIcon icon={faStar} className="icon favorite" />
-      </Link>
-      <Link to="/watchlist">
-        <FontAwesomeIcon icon={faPlay} className="icon favorite" />
-      </Link>
-      <Link to="/about">
-        <FontAwesomeIcon icon={faInfo} className="icon favorite" />
-      </Link>
-    </nav>
+    <header className='header'>
+      <nav className='navbar'>
+        <div className="navbar-container">
+          <ul className={`${listClassName} ${isActive ? "active" : ""}`}>
+            <li className="navbar-item">
+              <Link to="/" onClick={toggleActive} className='navlink'>
+                Home
+              </Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/favorite" onClick={toggleActive} className='navlink'>
+                Favorite
+              </Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/watchlist" onClick={toggleActive} className='navlink'>
+                Watchlist
+              </Link>
+            </li>
+          </ul>
+          <div className='navbar-icon' onClick={toggleActive}>
+            {isActive ? (
+                <div className={iconClassName}  onClick={toggleActive}>
+                  <FontAwesomeIcon icon={faXmark}/> {" "}
+                </div>          
+            ) : (
+                <div className={iconClassName} onClick={toggleActive}>
+                  <FontAwesomeIcon icon={faBars}/>
+                </div>                       
+            )}
+          </div>          
+        </div>
+      </nav>      
+    </header>
   )
 }
