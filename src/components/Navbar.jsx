bb6c1dc2e69b670951bb9995f7d8028a2cf024ff
Cassidy <cassidy.rouelle@gmail.com>
2024-04-18 14:46:14 +0200

    navbar fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,52 +12,48 @@ export default function Navbar() {
   const iconClassName = isMobile ? "toggle" : "no-icon";
 
   const toggleActive = () => setIsActive(!isActive);
-  const close = () => setIsActive(false);
-
+  
   return (
     <header className='header'>
-      <div className={`${isActive ? "main-container" : ""}`}>
         <nav className='navbar'>
-          <Link to="/">
-            <img src="/logo-long.svg" alt="logo TMDB" />
-          </Link>
           <div className="navbar-container">
-            <ul className={`${listClassName} ${isActive ? "active" : ""}`}>
-              <li>
-                <Link to="/" onClick={toggleActive} className='navlink'>
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link to="/favorite" onClick={toggleActive} className='navlink'>
-                  Favorite
-                </Link>
-              </li>
-              <li>
-                <Link to="/watchlist" onClick={toggleActive} className='navlink'>
-                  Watchlist
-                </Link>
-              </li>
-              <li>
-                <Link to="/genre" onClick={toggleActive} className='navlink'>
-                  Genre
-                </Link>
-              </li>
-            </ul>
             <div className='navbar-icon' onClick={toggleActive}>
-              {isActive ? (
-                  <div className={iconClassName}  onClick={toggleActive}>
-                    <FontAwesomeIcon icon={faXmark}/> {" "}
-                  </div>          
-              ) : (
-                  <div className={iconClassName} onClick={toggleActive}>
-                    <FontAwesomeIcon icon={faBars}/>
-                  </div>                       
-              )}
-            </div>          
+                {isActive ? (
+                    <div className={iconClassName}  onClick={toggleActive}>
+                      <FontAwesomeIcon icon={faXmark}/> {" "}
+                    </div>          
+                ) : (
+                    <div className={iconClassName} onClick={toggleActive}>
+                      <FontAwesomeIcon icon={faBars}/>
+                    </div>                       
+                )}
+            </div> 
+            <div className="navbar-container">
+              <ul className={`${listClassName} ${isActive ? "active" : ""}`}>
+                <li>
+                  <Link to="/" onClick={toggleActive} className='navlink'>
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/favorite" onClick={toggleActive} className='navlink'>
+                    Favorite
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/watchlist" onClick={toggleActive} className='navlink'>
+                    Watchlist
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/genre" onClick={toggleActive} className='navlink'>
+                    Genre
+                  </Link>
+                </li>
+              </ul>         
+            </div>
           </div>
         </nav>   
-      </div>
     </header>
   )
 }
