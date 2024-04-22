12b9ad8a6b80a17bf01f6df2e9767769dcd8708f
Cassidy <cassidy.rouelle@gmail.com>
2024-04-22 16:51:04 +0200

    api url

--- src/utils/request.jsx
+++ src/utils/request.jsx
@@ -1,4 +1,4 @@
-let API_URL = import.meta.env.VITE_REACT_APP_API_URL;
+let API_URL = "https://api.themoviedb.org/3/";
 let API_KEY = import.meta.env.VITE_REACT_APP_API_KEY;
 let ACCESS_TOKEN = import.meta.env.VITE_REACT_APP_ACCESS_TOKEN;
 
