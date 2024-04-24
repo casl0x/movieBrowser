e2ddbe5df93205dcce60e79886015070a2aa1fd7
Cassidy <cassidy.rouelle@gmail.com>
2024-04-24 15:06:45 +0200

    upcoming carousel

--- src/components/Upcoming.jsx
+++ src/components/Upcoming.jsx
@@ -1,8 +1,19 @@
 import React, { useState, useEffect } from 'react'
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 import { FetchUpcoming } from '../utils/request';
 
 export default function Upcoming() {
     const [movie, setMovie] = useState([]);
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    }
     
     useEffect(() => {
         const fetchComingSoon = async () => {
@@ -16,15 +27,17 @@ export default function Upcoming() {
         <div>
             <h1 className="title">Upcoming Movie</h1>
             <div>
-                {movie.map(m => (
-                    <div className="" key={m.id}>
-                        <img className="" src={`https://image.tmdb.org/t/p/w500${m.poster_path}`} alt={m.title} />
-                        <div className="">
-                            <h3 className="">{m.title}</h3>
-                            <p className="">{m.release_date}</p>
+                <Slider {...settings}>
+                    {movie.map(m => (
+                        <div key={m.id}>
+                            <img src={`https://image.tmdb.org/t/p/w500${m.poster_path}`} alt={m.title} />
+                            <div>
+                                <h3>{m.title}</h3>
+                                <p>{m.release_date}</p>
+                            </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </Slider>
             </div>
         </div>
     )
