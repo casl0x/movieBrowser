ee508cc225cce7e9fa06c2dbc18f7ebfe749c7ca
Cassidy <cassidy.rouelle@gmail.com>
2024-04-30 09:39:41 +0200

    add recommendation in movie page

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,9 +1,12 @@
 import MovieDetails from "../components/MovieDetails";
+import MovieRecommendation from "../components/MovieRecommendation";
 
 export default function Movie() {
   return (
     <>
       <MovieDetails />
+      <MovieRecommendation />
+
     </>
   )
 }
