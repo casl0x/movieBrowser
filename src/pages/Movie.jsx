0778e4ded66896762f784d8ce125ac7ba7aa056d
Cassidy <cassidy.rouelle@gmail.com>
2024-05-06 10:12:41 +0200

    .

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,20 +1,7 @@
 import MovieDetails from "../components/MovieDetails";
 import MovieRecommendation from "../components/MovieRecommendation";
-import { FetchMovie } from "../utils/request";
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
 
 export default function Movie() {
-  const [movie, setMovie] = useState(null)
-  const { movieId } = useParams()
-
-  useEffect(() => {
-    const fetchMovieDetails = async () => {
-        const movies = await FetchMovie(movieId);
-        setMovie(movies)
-    }
-    fetchMovieDetails()
-}, [movieId])
 
   return (
     <>
