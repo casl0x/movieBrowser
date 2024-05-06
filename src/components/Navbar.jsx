b5945ed8cffee931f724dbf63e9e727fe9c95156
Cassidy <cassidy.rouelle@gmail.com>
2024-05-06 09:49:51 +0200

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ export default function Navbar() {
   
   return (
     <header className='header'>
-        <img src="public/logo-long.svg" alt="" className='logo'/>
+        <img src="/logo-long.svg" alt="" className='logo'/>
         <nav className='navbar'>
           <div className="navbar-container">
             <div className='navbar-icon' onClick={toggleActive}>
