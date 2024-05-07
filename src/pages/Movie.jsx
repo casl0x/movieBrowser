0e70dc0950a1f859de89dd2d444583d5a23fc5d1
Cassidy <cassidy.rouelle@gmail.com>
2024-05-07 16:14:04 +0200

    .

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -5,13 +5,8 @@ export default function Movie() {
 
   return (
     <>
-<<<<<<< HEAD
         <MovieDetails />
         <MovieRecommendation />
-=======
-      <MovieDetails />
-      <MovieRecommendation />
->>>>>>> 0778e4ded66896762f784d8ce125ac7ba7aa056d
     </>
   )
 }
